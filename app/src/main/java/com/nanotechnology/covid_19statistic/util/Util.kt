6490dc8726bb0076624fcaa3467ceedd2e8a271f
Tiganimohammad <tiganimoh@gmail.com>
2019-10-10 16:24:45 +0200

    style text apperance and cards

--- app/src/main/java/com/nanotechnology/covid_19statistic/util/Util.kt
+++ app/src/main/java/com/nanotechnology/covid_19statistic/util/Util.kt
@@ -1,6 +1,11 @@
 package com.nanotechnology.covid_19statistic.util
 
 import android.annotation.SuppressLint
+import android.view.View
+import android.view.ViewGroup
+import android.view.WindowInsets
+import androidx.core.view.updateLayoutParams
+import androidx.databinding.BindingAdapter
 import java.text.SimpleDateFormat
 
 
@@ -19,4 +24,140 @@ import java.text.SimpleDateFormat
 fun convertLongToDateString(systemTime: Long): String {
     return SimpleDateFormat("EEEE MMM-dd-yyyy' Time: 'HH:mm")
             .format(systemTime).toString()
+}
+
+
+
+@BindingAdapter(
+    "paddingLeftSystemWindowInsets",
+    "paddingTopSystemWindowInsets",
+    "paddingRightSystemWindowInsets",
+    "paddingBottomSystemWindowInsets",
+    requireAll = false
+)
+fun View.applySystemWindowInsetsPadding(
+    previousApplyLeft: Boolean,
+    previousApplyTop: Boolean,
+    previousApplyRight: Boolean,
+    previousApplyBottom: Boolean,
+    applyLeft: Boolean,
+    applyTop: Boolean,
+    applyRight: Boolean,
+    applyBottom: Boolean
+) {
+    if (previousApplyLeft == applyLeft &&
+        previousApplyTop == applyTop &&
+        previousApplyRight == applyRight &&
+        previousApplyBottom == applyBottom
+    ) {
+        return
+    }
+
+    doOnApplyWindowInsets { view, insets, padding, _, _ ->
+        val left = if (applyLeft) insets.systemWindowInsetLeft else 0
+        val top = if (applyTop) insets.systemWindowInsetTop else 0
+        val right = if (applyRight) insets.systemWindowInsetRight else 0
+        val bottom = if (applyBottom) insets.systemWindowInsetBottom else 0
+
+        view.setPadding(
+            padding.left + left,
+            padding.top + top,
+            padding.right + right,
+            padding.bottom + bottom
+        )
+    }
+}
+
+@BindingAdapter(
+    "marginLeftSystemWindowInsets",
+    "marginTopSystemWindowInsets",
+    "marginRightSystemWindowInsets",
+    "marginBottomSystemWindowInsets",
+    requireAll = false
+)
+fun View.applySystemWindowInsetsMargin(
+    previousApplyLeft: Boolean,
+    previousApplyTop: Boolean,
+    previousApplyRight: Boolean,
+    previousApplyBottom: Boolean,
+    applyLeft: Boolean,
+    applyTop: Boolean,
+    applyRight: Boolean,
+    applyBottom: Boolean
+) {
+    if (previousApplyLeft == applyLeft &&
+        previousApplyTop == applyTop &&
+        previousApplyRight == applyRight &&
+        previousApplyBottom == applyBottom
+    ) {
+        return
+    }
+
+    doOnApplyWindowInsets { view, insets, _, margin, _ ->
+        val left = if (applyLeft) insets.systemWindowInsetLeft else 0
+        val top = if (applyTop) insets.systemWindowInsetTop else 0
+        val right = if (applyRight) insets.systemWindowInsetRight else 0
+        val bottom = if (applyBottom) insets.systemWindowInsetBottom else 0
+
+        view.updateLayoutParams<ViewGroup.MarginLayoutParams> {
+            leftMargin = margin.left + left
+            topMargin = margin.top + top
+            rightMargin = margin.right + right
+            bottomMargin = margin.bottom + bottom
+        }
+    }
+}
+
+fun View.doOnApplyWindowInsets(
+    block: (View, WindowInsets, InitialPadding, InitialMargin, Int) -> Unit
+) {
+    // Create a snapshot of the view's padding & margin states
+    val initialPadding = recordInitialPaddingForView(this)
+    val initialMargin = recordInitialMarginForView(this)
+    val initialHeight = recordInitialHeightForView(this)
+    // Set an actual OnApplyWindowInsetsListener which proxies to the given
+    // lambda, also passing in the original padding & margin states
+    setOnApplyWindowInsetsListener { v, insets ->
+        block(v, insets, initialPadding, initialMargin, initialHeight)
+        // Always return the insets, so that children can also use them
+        insets
+    }
+    // request some insets
+    requestApplyInsetsWhenAttached()
+}
+
+class InitialPadding(val left: Int, val top: Int, val right: Int, val bottom: Int)
+
+class InitialMargin(val left: Int, val top: Int, val right: Int, val bottom: Int)
+
+private fun recordInitialPaddingForView(view: View) = InitialPadding(
+    view.paddingLeft, view.paddingTop, view.paddingRight, view.paddingBottom
+)
+
+private fun recordInitialMarginForView(view: View): InitialMargin {
+    val lp = view.layoutParams as? ViewGroup.MarginLayoutParams
+        ?: throw IllegalArgumentException("Invalid view layout params")
+    return InitialMargin(lp.leftMargin, lp.topMargin, lp.rightMargin, lp.bottomMargin)
+}
+
+private fun recordInitialHeightForView(view: View): Int {
+    return view.layoutParams.height
+}
+
+fun View.requestApplyInsetsWhenAttached() {
+    if (isAttachedToWindow) {
+        // We're already attached, just request as normal
+        requestApplyInsets()
+    } else {
+        // We're not attached to the hierarchy, add a listener to
+        // request when we are
+        addOnAttachStateChangeListener(object : View.OnAttachStateChangeListener {
+            override fun onViewAttachedToWindow(v: View) {
+                v.removeOnAttachStateChangeListener(this)
+                v.requestApplyInsets()
+            }
+
+            override fun onViewDetachedFromWindow(v: View) = Unit
+        })
+    }
 }
